c8846870bb66f282dd4ecc6462edef92701e7091
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2024-03-06 13:29:09 +0100

    added wordpress form param

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 <!-- Search bar -->
 <section class="mt-10 w-[90%] mx-auto">
-    <form class="max-w-md mx-auto">
+    <form class="max-w-md mx-auto" action="<?= get_home_url(); ?>">
         <label for="default-search"
             class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div class="relative">
@@ -12,7 +12,7 @@
                         d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
             </div>
-            <input type="search" id="default-search"
+            <input name="s" type="search" id="default-search"
                 class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search Mockups, Logos..." required />
             <button type="submit"
